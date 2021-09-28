6e650c64e8ecaac05aefbdf35d4511d76009d48e
Aditya Choudhari <adityachoudhari26@gmail.com>
2021-09-27 20:13:11 -0400

    adding routing

--- hack/src/App.js
+++ hack/src/App.js
@@ -1,19 +1,20 @@
 import { version, Component } from 'inferno';
-import Logo from './logo';
+import { BrowserRouter, Route, Link } from 'inferno-router';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Logo width="80" height="80" />
-          <p>{`Welcome to Inferno ${version}`}</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/basic_calculator">Basic Calculator</Link></li>
+            <li><Link to="/expression_parser">Expression Parser</Link></li>
+          </ul>
+        </div>
+        <hr/>
+      </BrowserRouter>
     );
   }
 }
