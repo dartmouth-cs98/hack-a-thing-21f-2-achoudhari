4b6ee74f8d51f1cbc1cb0c76ff115204f8557576
Aditya Choudhari <adityachoudhari26@gmail.com>
2021-09-29 10:19:50 -0400

    updated README and implemented factorial functionality

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
-# Hack Technology / Project Attempted
+# Calculator and Expression Parser with Inferno.js
 
 
 ## What you built? 
 
-TODO: Short description of what you attempted to build
+In this project, we wanted to try and build out a feature using a Javascript library that is an alternative to React, Inferno.js. What interested us about this library was its claim from its documentation that it was one of the fastest libraries while still having a lot of functionality that React would provide. 
 
-Include some screenshots.
-[How?](https://help.github.com/articles/about-readmes/#relative-links-and-image-paths-in-readme-files)
+The website consisted of two main parts, a basic calculator and a rule based expression parser. 
 
 ## Who Did What?
 
-TODO: who worked on what part?
+Aditya - I worked on the expression validator and parser. This expression parser takes in a mathematical expression as input based on a set of rules I had predefined. It will first parse the expression and ensure that all parts of the expression, including nested expressions, are following these predefined rules. I think its cool because it recursively calculates the answer to the expression, and if there are any errors, will give a specific error and point the user to the specific part of the overall expression that the error is at. 
 
 ## What you learned
 
-TODO: what worked / what didn't work
+Before we could get to the features, we had to set up the general Inferno.js app with routing in order to have separate links to the two different components we worked on. One thing we noticed about Inferno.js is that it is extremly similar to React, and has pretty similar importing structure for external libraries. Routing was also pretty similar to React Routing, and utilized the `<BrowserRouter>` tag to create those links. There was also the regular `<Route>` tag which pointed to a specfic component. Overall, like previously stated, when we were actually coding we did not notice much difference between Inferno.js and React. In fact, we did not even notice much difference in speed between the two libraries though Inferno.js was marketed as being one of the fastest libraries. Though this might be true, our components were not nearly complex enough to yield demonstrable differences between the two libraries.
+
+Aditya - when I built out the rule based expression parser, what I wanted to build up was my skill in parsing and validating input. Parsing and validating input will likely be an important part of many CS 98 projects, so I wanted to build an expression parser that would be able to validate input and calculate any expression and also let users know where they may have erred when typing in an expression. 
 
 ## Authors
 
-TODO: list of authors
+Aditya Choudhari and Maxwell Carmichael
 
 ## Acknowledgments
 
-TODO: cite any tutorials followed here
+Aditya - I did not use any tutorial here, I just relied on the open source Inferno.js documentation that can be found here: [Inferno](https://www.infernojs.org/)
--- hack/src/ExpressionParser.js
+++ hack/src/ExpressionParser.js
@@ -1,8 +1,7 @@
 import { Component } from "inferno";
-import { exp } from "mathjs";
 
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const operations = [ '+', '-', '*', '/'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '!'];
+const operations = [ '+', '-', '*', '/', '%', '^'];
 
 class ExpressionParser extends Component{
   constructor(props) {
@@ -15,11 +14,19 @@ class ExpressionParser extends Component{
     }
   }
 
+  factorial = (n) => {
+    var res = 1;
+    for (let i = n; i > 0; i -= 1) {
+      res *= i;
+    }
+    return res;
+  }
+
   validateInput = (expression) => {
     if (expression.length < 7) {
       this.setState({ error: 'expression not long enough' });
     };
-    if (expression.charAt(0) !== '(' && expression.charAt(expression.length - 1) !== ')') {
+    if (expression.charAt(0) !== '(' || expression.charAt(expression.length - 1) !== ')') {
       this.setState({ error: `expression ${expression} does not start/end with valid parenthesis`})
       return false;
     }
@@ -35,9 +42,13 @@ class ExpressionParser extends Component{
 
     // find tokens
     var currIdx = 1;
-    if (expression.charAt(currIdx) === '(') {
+    if (expression.charAt(currIdx) === '(' || (expression.charAt(currIdx) === '!' && expression.charAt(currIdx + 1) === '(')) {
+      var factorial = false;
+      if (expression.charAt(currIdx) === '!') {
+        factorial = true;
+      }
       var balance = 1;
-      var tokenIdx = currIdx + 1;
+      var tokenIdx = factorial ? currIdx + 2 : currIdx + 1;
       while (balance !== 0 && tokenIdx !== expression.length - 1) {
         if (expression.charAt(tokenIdx) === '(') balance += 1;
         else if (expression.charAt(tokenIdx) === ')') balance -= 1;
@@ -48,11 +59,10 @@ class ExpressionParser extends Component{
         this.setState({ error: `invalid parentheses structure in first inner expression of ${expression}`});
         return false;
       } else {
-        const subExpression = expression.substring(currIdx, tokenIdx);
+        const subExpression = expression.substring(factorial ? currIdx + 1 : currIdx, tokenIdx);
         var subRes = this.validateInput(subExpression);
-
         if (subRes) {
-          numsToOperate.push(subRes);
+          numsToOperate.push(factorial ? this.factorial(Math.floor(subRes)) : subRes);
         } else {
           return false;
         }
@@ -60,10 +70,14 @@ class ExpressionParser extends Component{
       }
     } else {
       var numIdx = currIdx;
+      factorial = false;
+      if (expression.charAt(numIdx) === '!') {
+        factorial = true;
+      }
       while (numIdx < expression.length - 1 && numbers.includes(expression.charAt(numIdx))) {
         numIdx += 1;
       }
-      const numString = expression.substring(currIdx, numIdx);
+      const numString = expression.substring(factorial ? currIdx + 1 : currIdx, numIdx);
       var foundDecimal = false;
       for (let i = 0; i < numString.length; i++) {
         if (numString.charAt(i) === '.') {
@@ -78,7 +92,7 @@ class ExpressionParser extends Component{
         this.setState({ error: `number ${numString} cannot end in a decimal point`});
         return false;
       }
-      numsToOperate.push(parseFloat(numString));
+      numsToOperate.push(factorial ? this.factorial(parseInt(numString)) : parseFloat(numString));
       currIdx = numIdx;
     }
 
@@ -104,9 +118,13 @@ class ExpressionParser extends Component{
 
     currIdx += 1;
 
-    if (expression.charAt(currIdx) === '(') {
+    if (expression.charAt(currIdx) === '(' || (expression.charAt(currIdx) === '!' && expression.charAt(currIdx + 1) === '(')) {
+      factorial = false;
+      if (expression.charAt(currIdx) === '!') {
+        factorial = true;
+      }
       balance = 1;
-      tokenIdx = currIdx + 1;
+      tokenIdx = factorial ? currIdx + 2 : currIdx + 1;
       while (balance !== 0 && tokenIdx !== expression.length - 1) {
         if (expression.charAt(tokenIdx) === '(') balance += 1;
         else if (expression.charAt(tokenIdx) === ')') balance -= 1;
@@ -117,10 +135,10 @@ class ExpressionParser extends Component{
         this.setState({ error: `invalid parentheses structure in second inner expression of ${expression}`});
         return false;
       } else {
-        const subExpression = expression.substring(currIdx, tokenIdx);
+        const subExpression = expression.substring(factorial ? currIdx + 1 : currIdx, tokenIdx);
         subRes = this.validateInput(subExpression);
         if (subRes) {
-          numsToOperate.push(subRes);
+          numsToOperate.push(factorial ? this.factorial(Math.floor(subRes)) : subRes);
         } else {
           return false;
         }
@@ -128,11 +146,15 @@ class ExpressionParser extends Component{
       }
     } else {
       numIdx = currIdx;
+      factorial = false;
+      if (expression.charAt(numIdx) === '!') {
+        factorial = true;
+      }
       while (numIdx < expression.length - 1 && numbers.includes(expression.charAt(numIdx))) {
         numIdx += 1;
       }
+      const numString = expression.substring(factorial ? currIdx + 1 : currIdx, numIdx);
 
-      const numString = expression.substring(currIdx, numIdx);
       foundDecimal = false;
       for (let i = 0; i < numString.length; i++) {
         if (numString.charAt(i) === '.') {
@@ -147,12 +169,17 @@ class ExpressionParser extends Component{
         this.setState({ error: `number ${numString} cannot end in a decimal point`});
         return false;
       }
-      numsToOperate.push(parseFloat(numString));
+      numsToOperate.push(factorial ? this.factorial(parseInt(numString)) : parseFloat(numString));
       currIdx = numIdx;
     }
 
-    if (expression.charAt(currIdx) !== ')' && currIdx !== expression.length - 1) {
+    if (expression.charAt(currIdx) !== ')') {
       this.setState({ error: `last character in expression ${expression} is not ')'`})
+      return false;
+    }
+
+    if (currIdx !== expression.length - 1) {
+      this.setState({ error: `${expression} has extra characters at the end`});
       return false;
     }
 
@@ -162,22 +189,52 @@ class ExpressionParser extends Component{
       return numsToOperate[0] - numsToOperate[1];
     } else if (operation === '*') {
       return numsToOperate[0] * numsToOperate[1];
-    } else {
+    } else if (operation === '/') {
       return numsToOperate[0] / numsToOperate[1];
+    } else if (operation === '%') {
+      return numsToOperate[0] % numsToOperate[1];
+    } else if (operation === '^') {
+      return Math.pow(numsToOperate[0], numsToOperate[1])
     }
   }
 
   render = () => {
     return (
-      <div>
-        <input value={this.state.expression} onInput={(e) => { this.setState({expression: e.target.value})}}></input>
-        <button onClick={() => {
-          this.setState({answer: null, error: null})
-          const res = this.validateInput(this.state.expression);
-          if (res !== false) {
-            this.setState({ answer: res });
-          }
-        }}>Calculate!</button>
+      <div style={{display: "flex", "flex-direction": "column"}}>
+        <div>Input an expression according to these rules and see the result</div>
+        <ul>
+          <li>Expression starts and ends with opening and closing parentheses</li>
+          <li>There is a space between the two expressions and the operator</li>
+          <li>The operator is either +, -, *, /, ^, or %</li>
+          <li>There is only one operator per expression</li>
+          <li>Factorials must come before the expression i.e. !(8+5)</li>
+          <li>You can include nested expressions, which must follow the same rules listed</li>
+        </ul>
+        <div>Valid examples</div>
+        <ul>
+          <li>(8 + 5)</li>
+          <li>((8 + 5) * 3)</li>
+          <li>((7.23 / 2.56) * (5 - 2.46))</li>
+          <li>(!(2 + 3) % (5 + !3))</li>
+        </ul>
+        <div>Invalid examples will trigger errors (which we display to help you fix)</div>
+        <ul>
+          <li>5 + 7 --{">"} does not start or end with parentheses</li>
+          <li> (8 + (7 *5)) --{">"} no space between expressions and operators</li>
+          <li>(8 + ((2 + 3) --{">"} invalid parentheses structure</li>
+          <li>(6.2. + 5) --{">"} invalid decimals</li>
+          <li>(8.5 + d) --{">"} invalid character in expression</li>
+        </ul>
+        <div>
+          <input value={this.state.expression} onInput={(e) => { this.setState({expression: e.target.value})}}></input>
+          <button onClick={() => {
+            this.setState({answer: null, error: null})
+            const res = this.validateInput(this.state.expression);
+            if (res !== false) {
+              this.setState({ answer: res });
+            }
+          }}>Calculate!</button>
+        </div>
         {!!this.state.answer && <div>{this.state.answer}</div>}
         {!!this.state.error && <div style={{color: "red"}}>{this.state.error}</div>}
       </div>
